bd83d6dcd54b06f23d7e6235afc2494b92647336
lee <lbsoo1021@naver.com>
2022-08-10 14:28:05 +0900

    djust Django course 8

--- accountApp/views.py
+++ accountApp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello world')
+    return render(request, 'base.html')
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -11,16 +11,26 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ, os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR, '.env')
+    )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q2uh5rt1(kwlai%5ezh3rc71w1rqp_k$&^uvm22!f)^!taf$=w'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +65,7 @@ ROOT_URLCONF = 'pragmatic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
