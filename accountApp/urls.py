28bcc925457902b6237a152f1054a2649c0afdec
lee <lbsoo1021@naver.com>
2022-08-17 17:27:34 +0900

    django course 21 commit

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -1,9 +1,11 @@
 import imp
 from django.urls import path
 from accountApp.views import hello_world
+from .views import AccountCreateView
 
 app_name = 'accountApp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,8 +1,11 @@
-
-from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+from django.views.generic import CreateView
+from django.urls import reverse, reverse_lazy
+
 from accountApp.models import HelloWorld
 
 def hello_world(request):
@@ -19,3 +22,9 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountApp/hello_world.html', context={'hello_world_list' : hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountApp:hello_world')
+    template_name = 'accountApp/create.html'
