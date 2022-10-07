b723f39544a9d351f83ae612cbe38232621f8643
lee <lbsoo1021@naver.com>
2022-10-08 02:56:57 +0900

    django course 33 commit

--- profileApp/views.py
+++ profileApp/views.py
@@ -3,7 +3,7 @@ from django.views.generic import CreateView, UpdateView
 from .models import Profile
 from .forms import ProfileCreatioinForm
 
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 
 from django.utils.decorators import method_decorator
 from .decorators import profile_ownership_required
@@ -14,7 +14,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreatioinForm
-    success_url = reverse_lazy('accountApp:hello_world')
     template_name = 'profileApp/create.html'
 
     def form_valid(self, form):
@@ -23,11 +22,16 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountApp:detail', kwargs={'pk': self.object.user.pk})
+
 @method_decorator(profile_ownership_required, 'get')
-@method_decorator(profile_ownership_required, 'post')
+# @method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreatioinForm
-    success_url = reverse_lazy('accountApp:hello_world')
     template_name = 'profileApp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountApp:detail', kwargs={'pk': self.object.user.pk})
