4f1cbc5158ef9b6019e793106463955872c9b3cb
lee <lbsoo1021@naver.com>
2023-03-07 00:17:33 +0900

    change accountCreate redirect

--- accountApp/views.py
+++ accountApp/views.py
@@ -21,7 +21,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountApp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountApp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
